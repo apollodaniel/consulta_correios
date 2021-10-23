cb22547866a19af97e3c28e77ceafec503511b94
Apollo <farofadecachorro@gmail.com>
2021-10-23 13:37:10 -0300

    Added load activity to load the package before show the result

---
+++ app/src/main/java/com/apollo/consulta_correios/ActivityLoadPackage.java
@@ -0,0 +1,14 @@
+package com.apollo.consulta_correios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityLoadPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_package);
+    }
+}
