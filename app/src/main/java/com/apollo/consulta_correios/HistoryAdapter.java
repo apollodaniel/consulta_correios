aa774df07f4a0ef992dc38b3aa656146c577ffa9
Apollo <farofadecachorro@gmail.com>
2021-10-23 15:40:33 -0300

    All working fine

--- app/src/main/java/com/apollo/consulta_correios/HistoryAdapter.java
+++ app/src/main/java/com/apollo/consulta_correios/HistoryAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -58,5 +59,8 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.viewHold
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
+    }
+    public interface OnHistoryListener{
+        void onNoteClick(int position);
     }
 }
