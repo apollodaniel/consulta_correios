b72323df6077e1c67cb453e9d4d986672d4cea92
Apollo <farofadecachorro@gmail.com>
2021-10-24 13:41:20 -0300

    Ui improved

--- app/src/main/java/com/apollo/consulta_correios/ActivityInicio.java
+++ app/src/main/java/com/apollo/consulta_correios/ActivityInicio.java
@@ -8,9 +8,11 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +25,8 @@ public class ActivityInicio extends AppCompatActivity {
     EditText edtCodigoProduto;
     @BindView(R.id.activity_inicio_btn_historico)
     Button btnHistorico;
+    @BindView(R.id.activity_inicio_imgbg)
+    ImageView imgBg;
 
     private final String SHARED_PREFS = "prefs";
     private final String PACKAGE_QUERY_KEY = "package";
@@ -36,6 +40,10 @@ public class ActivityInicio extends AppCompatActivity {
         setContentView(R.layout.activity_inicio);
 
         ButterKnife.bind(ActivityInicio.this);
+
+        Drawable drawable = getDrawable(R.mipmap.hands_bg);
+        imgBg.setImageDrawable(drawable);
+        imgBg.setAlpha(0.8f);
 
         SharedPreferences sh = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         String package_codes = sh.getString(PACKAGE_QUERY_KEY, "");
